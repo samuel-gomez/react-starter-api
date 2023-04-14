0ed467a14f4b3ea795ee3bee75e2f8298b36143a
Samuel Gomez <contact@samuelgomez.fr>
2023-04-14 13:59:28 +0200

    add pug render view

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+
+/* for dirname ES module */
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/* for dirname ES module */
+
 const { PORT = '80' } = process.env;
 import { API } from './api/constants.js';
 import {
@@ -23,15 +31,28 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
-router.get('/members', members);
-router.get('/members/search', membersSearch);
-router.get('/members/:id', membersDetail);
-router.get('/members/:id/download-detail', downloadDetails);
-router.get('/people', people);
-router.get('/convert/:base_currency', convert);
+router.get(`/members`, members);
+router.get(`/members/search`, membersSearch);
+router.get(`/members/:id`, membersDetail);
+router.get(`/members/:id/download-detail`, downloadDetails);
+router.get(`/people`, people);
+router.get(`/convert/:base_currency`, convert);
 
 app.use(`/${API}`, router);
+
+/* exemple render page */
+app.get('/', (req, res) => {
+  res.render('pages/index', { title: 'Accueil' });
+});
+
+/* exemple render page about */
+app.get('/about', (req, res) => {
+  res.render('pages/about', { title: 'About' });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
