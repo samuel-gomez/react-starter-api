5b186fc10eff723648d2e5f3f0ab704bf8c29cc8
samuel.gomez <contact@samuel.gomez.fr>
2023-09-22 10:24:41 +0200

    feat(prod): origin whitelist 2

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", whitelist);
+  res.header("Access-Control-Allow-Credentials", true);
   // res.header("Access-Control-Allow-Headers", "x-sso");
   // res.header("Access-Control-Expose-Headers", "x-sso");
 
